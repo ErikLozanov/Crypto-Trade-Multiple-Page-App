3d343392652aca65d267ddbe1e2079058c9d20fb
Erik Lozanov <122800063+ErikLozanov@users.noreply.github.com>
2023-10-12 23:17:13 +0300

    Added login view

--- routes.js
+++ routes.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const homeContoller = require('./controllers/homeController');
+const authController = require('./controllers/authController');
 
 router.use(homeContoller);
+router.use(authController);
 
 module.exports = router;
