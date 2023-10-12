a69f058c142ac69f5b502f5f8bb60ecfb164530b
Erik Lozanov <122800063+ErikLozanov@users.noreply.github.com>
2023-10-13 00:45:33 +0300

    Added authentication middleware

---
+++ middlewares/authenticationMiddleware.js
@@ -0,0 +1,19 @@
+
+const jwt = require('../lib/jsonwebtoken');
+
+exports.authentication =async (req, res, next) => {
+    const token = req.cookies['auth'];
+
+    if(token) {
+        try {
+            const decodedToken = await jwt.verify(token);
+            
+            req.user = decodedToken;
+        } catch (err) {
+            res.clearCookie('auth');
+            return res.status(401).render('home/404');
+        }
+    } 
+    
+    next();
+};
