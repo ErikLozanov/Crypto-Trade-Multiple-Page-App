6d96a9f1746f32760ec8f3992f6e85d03733bd86
Erik Lozanov <122800063+ErikLozanov@users.noreply.github.com>
2023-10-13 00:08:55 +0300

    Add user already exists validation

--- services/authService.js
+++ services/authService.js
@@ -1,5 +1,8 @@
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
+
+exports.findByUsername = (username) => User.findOne({username});
+
 exports.register = async (username, email, password, repeatPassword) => {
     // Validate password
     if(password !== repeatPassword) {
@@ -7,7 +10,10 @@ exports.register = async (username, email, password, repeatPassword) => {
     }
 
     // TODO: Check if user exists
-
+    const existingUser =await this.findByUsername(username);
+    if(existingUser) {
+        throw new Error('User already exists!');
+    }
 
     // TODO: Validate password
 
