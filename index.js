e2246520c900f20dc4773c2a16d342f72835d6a7
Erik Lozanov <wolfierecordss@gmail.com>
2023-10-17 18:39:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ app.use(cookieParser());
 app.use(authentication);
 app.use(routes);
 
-mongoose.connect('mongodb://127.0.0.1:27017/crypto');
+mongoose.connect('mongodb://127.0.0.1:27017/crypto')
+.then(() => console.log('DB Connected Successfully!'))
+.catch((err) => console.log(err.message));
 
 app.listen(5000,() => console.log('Server is running on port 5000...'));
