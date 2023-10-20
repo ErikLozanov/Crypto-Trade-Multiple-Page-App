eeefc00cd5623d263578f5e265cceda1c41b5ff1
Erik Lozanov <wolfierecordss@gmail.com>
2023-10-21 00:12:18 +0300

    Added  Crypto model

---
+++ models/Crypto.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+
+const cryptoSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    payment: {
+        type: String,
+        required: true
+    },
+    owner: {
+        type: mongoose.Types.ObjectId,
+        ref: "User",
+    }
+});
+
+
+const Crypto = mongoose.model('Crypto', cryptoSchema);
+
+module.exports = Crypto;
