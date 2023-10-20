48a545f4be89066d57d1a82041e96ff963ab46ee
Erik Lozanov <wolfierecordss@gmail.com>
2023-10-21 00:12:47 +0300

    Added create view

--- routes.js
+++ routes.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const homeContoller = require('./controllers/homeController');
 const authController = require('./controllers/authController');
+const cryptoController = require('./controllers/cryptoController');
 
 router.use(homeContoller);
 router.use(authController);
+router.use('/cryptos',cryptoController);
 
 module.exports = router;
