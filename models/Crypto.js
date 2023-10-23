6d8b8b3f3563366fcd2cb01c9f7c261091c55231
Erik Lozanov <wolfierecordss@gmail.com>
2023-10-23 21:25:47 +0300

    Update Crypto.js

--- models/Crypto.js
+++ models/Crypto.js
@@ -16,7 +16,7 @@ const cryptoSchema = new mongoose.Schema({
     },
     description: {
         type: String,
-        required: true
+        required: [true,'Description is required']
     },
     payment: {
         type: String,
